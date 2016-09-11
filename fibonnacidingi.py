cc5e9f0ae16dd77d8fc6d32264befc2e7954910f
seeglem <seeglem@yahoo.com>
2016-09-10 23:02:57 -0500

    Add files via upload

---
+++ fibonnacidingi.py
@@ -0,0 +1,18 @@
+#hashtag
+#created by Hunter Seglem, Jon Staggs, with Counselling from Caleb Barnwell, this is Hunter & Jon's first day of coding.
+
+import math
+
+def fibPrint(n):
+	a=0
+	b=1
+	for i in range(n-1):
+		c=a
+		a=b
+		b=b+c
+		print(a)
+
+kCzech=input("Just put a damn number here and see what happens ")
+number=int(kCzech)
+
+fibPrint(number)
